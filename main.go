b1475dac52b068aae79f37ddc518a31129d20dd0
Christopher Davies <dev@christophilus.com>
2022-02-12 11:37:54 -0500

    More realistic simulation

--- main.go
+++ main.go
@@ -10,11 +10,20 @@ import (
 	"log"
 	"os"
 	"path"
+	"strconv"
 	"sync"
 	"time"
 
 	_ "github.com/mattn/go-sqlite3"
 )
+
+type Task struct {
+	Id     string
+	Output string
+	Status string
+	Cmd    string
+	Args   []string
+}
 
 func genId() string {
 	b := make([]byte, 16)
@@ -24,158 +33,268 @@ func genId() string {
 	return hex.EncodeToString(b)
 }
 
-// Task is a basic struct. Realistically, we'd be storing a more complex
-// structure, but I think this will be sufficient for a basic test.
-type Task struct {
-	Id   string
-	Cmd  string
-	Args []string
+func simulate(fn func(action string, t *Task)) {
+	t := &Task{
+		Id:     genId(),
+		Status: "pending",
+		Cmd:    "example",
+		Output: "",
+		Args:   []string{"a", "b", "c"},
+	}
+
+	fn("new", t)
+	t.Status = "processing"
+	fn("status", t)
+	for i := 0; i < 10; i++ {
+		t.Output = strconv.Itoa(i) + "Some kind of output and whatever"
+		fn("out", t)
+	}
+	fn("rm", t)
 }
 
-func writeNConcurrent(name string, concurency, num int, fn func(id string, js []byte) error) {
+func logPanic(err error) {
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+func makeSQLSimulator() (*sql.DB, func(action string, t *Task)) {
+	fmt.Println("Opening...")
+	db, err := sql.Open("sqlite3", "./tmp.db?_timeout=5000&_journal=WAL&_sync=1")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	fmt.Println("Migrations...")
+	_, err = db.Exec(`
+	  BEGIN;
+		CREATE TABLE IF NOT EXISTS tasks (
+			id      varchar(16) NOT NULL PRIMARY KEY,
+			status  varchar(16) NOT NULL,
+			cmd     varchar(16) NOT NULL,
+			args    text NOT NULL,
+			output  text NULL
+		);
+		CREATE INDEX IF NOT EXISTS task_status_idx ON tasks (status);
+		COMMIT;
+	`)
+	logPanic(err)
+
+	return db, func(action string, t *Task) {
+		switch action {
+		case "new":
+			args, err := json.Marshal(t.Args)
+			logPanic(err)
+			_, err = db.Exec(`INSERT INTO tasks
+				(id, status, cmd, args)
+			VALUES
+			  (@id, @status, @cmd, @args)
+			`, sql.Named("id", t.Id), sql.Named("status", t.Status), sql.Named("cmd", t.Cmd), sql.Named("args", string(args)))
+			logPanic(err)
+		case "status":
+			_, err = db.Exec(`UPDATE tasks SET status=@status WHERE id=@id`, sql.Named("id", t.Id), sql.Named("status", t.Status))
+			logPanic(err)
+		case "out":
+			_, err = db.Exec(`UPDATE tasks SET output=@out WHERE id=@id`, sql.Named("id", t.Id), sql.Named("out", t.Output))
+			logPanic(err)
+		case "rm":
+			_, err = db.Exec(`DELETE FROM tasks WHERE id=@id`, sql.Named("id", t.Id))
+			logPanic(err)
+		}
+	}
+}
+
+func fileSimulator(action string, t *Task) {
+	dir := "./tmp"
+	if action == "rm" {
+		if err := os.Remove(path.Join(dir, t.Id)); err != nil {
+			log.Panic(err)
+		}
+		return
+	}
+
+	b, err := json.Marshal(t)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	err = ioutil.WriteFile(path.Join(dir, t.Id), b, 0777)
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+func simulateN(name string, concurrency, num int, handler func(action string, t *Task)) {
 	wg := new(sync.WaitGroup)
 	start := time.Now()
-	wg.Add(concurency)
-	for i := 0; i < concurency; i++ {
+	wg.Add(concurrency)
+	for i := 0; i < concurrency; i++ {
 		go func() {
 			defer wg.Done()
 			for i := 0; i < num; i++ {
-				t := &Task{
-					Id:   genId(),
-					Cmd:  "example",
-					Args: []string{"a", "b", "c"},
-				}
-				b, err := json.Marshal(t)
-				if err != nil {
-					log.Panic(err)
-				}
-				if err = fn(t.Id, b); err != nil {
-					log.Panic(err)
-				}
+				simulate(handler)
 			}
 		}()
 	}
 	wg.Wait()
-	fmt.Println(name, "wrote", num*concurency, "tasks in ", time.Since(start).String())
+	fmt.Println(name, "ran", num*concurrency, "tasks in ", time.Since(start).String())
 }
 
-func writeN(name string, num int, fn func(id string, js []byte) error) {
-	start := time.Now()
+// // Task is a basic struct. Realistically, we'd be storing a more complex
+// // structure, but I think this will be sufficient for a basic test.
+// type Task struct {
+// 	Id   string
+// 	Cmd  string
+// 	Args []string
+// }
 
-	for i := 0; i < num; i++ {
-		t := &Task{
-			Id:   genId(),
-			Cmd:  "example",
-			Args: []string{"a", "b", "c"},
-		}
-		b, err := json.Marshal(t)
-		if err != nil {
-			log.Panic(err)
-		}
-		if err = fn(t.Id, b); err != nil {
-			log.Panic(err)
-		}
-	}
-	fmt.Println(name, "wrote", num, "tasks in ", time.Since(start).String())
-}
+// func writeNConcurrent(name string, concurency, num int, fn func(id string, js []byte) error) {
+// 	wg := new(sync.WaitGroup)
+// 	start := time.Now()
+// 	wg.Add(concurency)
+// 	for i := 0; i < concurency; i++ {
+// 		go func() {
+// 			defer wg.Done()
+// 			for i := 0; i < num; i++ {
+// 				t := &Task{
+// 					Id:   genId(),
+// 					Cmd:  "example",
+// 					Args: []string{"a", "b", "c"},
+// 				}
+// 				b, err := json.Marshal(t)
+// 				if err != nil {
+// 					log.Panic(err)
+// 				}
+// 				if err = fn(t.Id, b); err != nil {
+// 					log.Panic(err)
+// 				}
+// 			}
+// 		}()
+// 	}
+// 	wg.Wait()
+// 	fmt.Println(name, "wrote", num*concurency, "tasks in ", time.Since(start).String())
+// }
 
-func writeFiles(num int) {
-	dir := "./tmp"
-	// Don't really care about errors for testing... we'll know if it fails
-	os.Mkdir(dir, 0777)
-	writeN("Files", num, func(id string, js []byte) error {
-		return ioutil.WriteFile(path.Join(dir, id), js, 0777)
-	})
-}
+// func writeN(name string, num int, fn func(id string, js []byte) error) {
+// 	start := time.Now()
 
-func writeConcurrentFiles(concurrency, num int) {
-	dir := "./tmp"
-	// Don't really care about errors for testing... we'll know if it fails
-	os.Mkdir(dir, 0777)
-	writeNConcurrent("Files", concurrency, num, func(id string, js []byte) error {
-		return ioutil.WriteFile(path.Join(dir, id), js, 0777)
-	})
-}
+// 	for i := 0; i < num; i++ {
+// 		t := &Task{
+// 			Id:   genId(),
+// 			Cmd:  "example",
+// 			Args: []string{"a", "b", "c"},
+// 		}
+// 		b, err := json.Marshal(t)
+// 		if err != nil {
+// 			log.Panic(err)
+// 		}
+// 		if err = fn(t.Id, b); err != nil {
+// 			log.Panic(err)
+// 		}
+// 	}
+// 	fmt.Println(name, "wrote", num, "tasks in ", time.Since(start).String())
+// }
 
-func writeConcurrentSql(concurrency, num int) {
-	fmt.Println("Opening...")
-	db, err := sql.Open("sqlite3", "./tmp.db?_timeout=5000&_journal=WAL&_sync=1")
-	if err != nil {
-		log.Panic(err)
-	}
-	defer db.Close()
+// func writeFiles(num int) {
+// 	dir := "./tmp"
+// 	// Don't really care about errors for testing... we'll know if it fails
+// 	os.Mkdir(dir, 0777)
+// 	writeN("Files", num, func(id string, js []byte) error {
+// 		return ioutil.WriteFile(path.Join(dir, id), js, 0777)
+// 	})
+// }
 
-	fmt.Println("Migrations...")
-	_, err = db.Exec(`
-		CREATE TABLE IF NOT EXISTS tasks (
-			id    varchar(16) NOT NULL PRIMARY KEY,
-			task  text NOT NULL
-		)
-	`)
+// func writeConcurrentFiles(concurrency, num int) {
+// 	dir := "./tmp"
+// 	// Don't really care about errors for testing... we'll know if it fails
+// 	os.Mkdir(dir, 0777)
+// 	writeNConcurrent("Files", concurrency, num, func(id string, js []byte) error {
+// 		return ioutil.WriteFile(path.Join(dir, id), js, 0777)
+// 	})
+// }
 
-	if err != nil {
-		log.Panic(err)
-	}
+// func writeConcurrentSql(concurrency, num int) {
+// 	fmt.Println("Opening...")
+// 	db, err := sql.Open("sqlite3", "./tmp.db?_timeout=5000&_journal=WAL&_sync=1")
+// 	if err != nil {
+// 		log.Panic(err)
+// 	}
+// 	defer db.Close()
 
-	w, err := db.Prepare(`
-		INSERT INTO tasks
-			(id, task)
-		VALUES
-			(@id, @task)
-	`)
-	if err != nil {
-		log.Panic(err)
-	}
-	defer w.Close()
+// 	fmt.Println("Migrations...")
+// 	_, err = db.Exec(`
+// 		CREATE TABLE IF NOT EXISTS tasks (
+// 			id    varchar(16) NOT NULL PRIMARY KEY,
+// 			task  text NOT NULL
+// 		)
+// 	`)
 
-	writeNConcurrent("SQLite", concurrency, num, func(id string, js []byte) error {
-		_, err := w.Exec(sql.Named("id", id), sql.Named("task", string(js)))
-		return err
-	})
-}
+// 	if err != nil {
+// 		log.Panic(err)
+// 	}
 
-func writeSql(num int) {
-	fmt.Println("Opening...")
-	db, err := sql.Open("sqlite3", "./tmp.db?_timeout=5000&_journal=WAL&_sync=1")
-	if err != nil {
-		log.Panic(err)
-	}
-	defer db.Close()
+// 	w, err := db.Prepare(`
+// 		INSERT INTO tasks
+// 			(id, task)
+// 		VALUES
+// 			(@id, @task)
+// 	`)
+// 	if err != nil {
+// 		log.Panic(err)
+// 	}
+// 	defer w.Close()
 
-	fmt.Println("Migrations...")
-	_, err = db.Exec(`
-		CREATE TABLE IF NOT EXISTS tasks (
-			id    varchar(16) NOT NULL PRIMARY KEY,
-			task  text NOT NULL
-		)
-	`)
+// 	writeNConcurrent("SQLite", concurrency, num, func(id string, js []byte) error {
+// 		_, err := w.Exec(sql.Named("id", id), sql.Named("task", string(js)))
+// 		return err
+// 	})
+// }
 
-	if err != nil {
-		log.Panic(err)
-	}
+// func writeSql(num int) {
+// 	fmt.Println("Opening...")
+// 	db, err := sql.Open("sqlite3", "./tmp.db?_timeout=5000&_journal=WAL&_sync=1")
+// 	if err != nil {
+// 		log.Panic(err)
+// 	}
+// 	defer db.Close()
 
-	w, err := db.Prepare(`
-		INSERT INTO tasks
-			(id, task)
-		VALUES
-			(@id, @task)
-	`)
-	if err != nil {
-		log.Panic(err)
-	}
-	defer w.Close()
+// 	fmt.Println("Migrations...")
+// 	_, err = db.Exec(`
+// 		CREATE TABLE IF NOT EXISTS tasks (
+// 			id    varchar(16) NOT NULL PRIMARY KEY,
+// 			task  text NOT NULL
+// 		)
+// 	`)
 
-	writeN("SQLite", num, func(id string, js []byte) error {
-		_, err := w.Exec(sql.Named("id", id), sql.Named("task", string(js)))
-		return err
-	})
-}
+// 	if err != nil {
+// 		log.Panic(err)
+// 	}
+
+// 	w, err := db.Prepare(`
+// 		INSERT INTO tasks
+// 			(id, task)
+// 		VALUES
+// 			(@id, @task)
+// 	`)
+// 	if err != nil {
+// 		log.Panic(err)
+// 	}
+// 	defer w.Close()
+
+// 	writeN("SQLite", num, func(id string, js []byte) error {
+// 		_, err := w.Exec(sql.Named("id", id), sql.Named("task", string(js)))
+// 		return err
+// 	})
+// }
 
 func main() {
-	writeConcurrentFiles(100, 1000)
-	writeConcurrentSql(100, 1000)
-	writeConcurrentFiles(100, 1000)
-	writeConcurrentSql(100, 1000)
-	writeConcurrentFiles(100, 1000)
-	writeConcurrentSql(100, 1000)
+	os.Mkdir("./tmp", 0777)
+	db, sqlHandler := makeSQLSimulator()
+	defer db.Close()
+	simulateN("SQL", 100, 100, sqlHandler)
+	simulateN("FILE", 100, 100, fileSimulator)
+	simulateN("SQL", 100, 100, sqlHandler)
+	simulateN("FILE", 100, 100, fileSimulator)
+	simulateN("SQL", 100, 100, sqlHandler)
+	simulateN("FILE", 100, 100, fileSimulator)
 }
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 # DBENCH
 
-Basic benchmarks for SQLite vs filesystem (btrfs on a 2020 Dell XPS SSD).
+Basic benchmarks for SQLite vs file system (btrfs on a 2020 Dell XPS SSD).
 
 ## Linear writes
 
@@ -8,18 +8,18 @@ Basic benchmarks for SQLite vs filesystem (btrfs on a 2020 Dell XPS SSD).
 
 ```
 SQLite      647ms, 809ms, 708ms
-FileSystem  393ms, 371ms, 382ms
+BTRFS       393ms, 371ms, 382ms
 ```
 
 For sequential writes, the file system is 1.8x faster.
 
 ## Concurrent writes
 
-Here, we're going to crank up the concurrency and have 100 concurrent writers, each writing 1K records. SQLite will serialize these under the hood. The filesystem will do whatever it does.
+Here, we're going to crank up the concurrency and have 100 concurrent writers, each writing 1K records. SQLite will serialize these under the hood. The file system will do whatever it does.
 
 ```
 SQLite        8.2s, 8.1s, 8.5s
-FileSystem    3.1s, 3.4s, 3.4s
+BTRFS         3.1s, 3.4s, 3.4s
 ```
 
 For concurrent writes, the file system is 2.4x faster.
@@ -35,4 +35,14 @@ The real-world application I'll be writing would have a bit more structure and m
 
 For this, we'll want to index by status, and in the real world, I'd *probably* also index by `scheduled_at` so that we can handle job scheduling efficiently. If I were to use the file system for this, I'd keep the queues and schedules in memory, and rebuild it when the application starts. I've tested the in-memory approach, and it's blisteringly fast (millions of ops per second, given proper care).
 
+Concurrency: 100, each running a simulation of 100 tasks (create, change status, update "output" 10 times, delete):
+
+```
+SQLite      11.5s, 10.3s, 7.8
+BTRFS       3.3s, 3.2s, 2.4s
+```
+
+In this test, the file system was 3.3x faster than SQLite. This surprises me, since we're writing the *entire* file each time vs SQLite presumably being able to do more optimal, in-place updates (though a variety of things may mean that's not actually happening).
+
+Let's try again. This time, we'll do proper file writes (write to a tmp file, rename to overwrite the current file). This should be a *bit* more crash-resistant, though for my use case, it probably doesn't matter a whole lot if one or two tasks fail due to crashes once or twice a year.
 
